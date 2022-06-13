fdbd023ea7bbf86f2df71c887907393a31a2f3d2
Roman Sofin <roma.sofin@gmail.com>
2022-06-13 20:07:34 +0300

    Add count-parents in json file

---
+++ 3-4-csv-and-json/count-parents.py
@@ -0,0 +1,3 @@
+json_test1 = [{"name": "A", "parents": []}, {"name": "B", "parents": ["A", "C"]}, {"name": "C", "parents": ["A"]}]
+json_test2 = [{"name": "B", "parents": ["A", "C"]}, {"name": "C", "parents": ["A"]}, {"name": "A", "parents": []},
+              {"name": "D", "parents": ["C", "F"]}, {"name": "E", "parents": ["D"]}, {"name": "F", "parents": []}]
