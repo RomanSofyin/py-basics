26cfebb7bbbf94f0c16dd5af422fa713b35f14dc
Roman Sofyin <roman.sofyin@dell.com>
2022-06-15 20:57:48 +0300

    Approaching to 3-4 count parents in json

--- 3-4-csv-and-json/count-parents.py
+++ 3-4-csv-and-json/count-parents.py
@@ -1,3 +1,26 @@
-json_test1 = [{"name": "A", "parents": []}, {"name": "B", "parents": ["A", "C"]}, {"name": "C", "parents": ["A"]}]
-json_test2 = [{"name": "B", "parents": ["A", "C"]}, {"name": "C", "parents": ["A"]}, {"name": "A", "parents": []},
-              {"name": "D", "parents": ["C", "F"]}, {"name": "E", "parents": ["D"]}, {"name": "F", "parents": []}]
+import json
+
+json_test1 = r'[{"name": "A", "parents": []}, {"name": "B", "parents": ["A", "C"]}, {"name": "C", "parents": ["A"]}]'
+json_test2 = r'['                                      \
+             r'  { "name":"B", "parents":["A","C"] },' \
+             r'  { "name":"C", "parents":["A"]     },' \
+             r'  { "name":"A", "parents":[]        },' \
+             r'  { "name":"D", "parents":["C","F"] },' \
+             r'  { "name":"E", "parents":["D"]     },' \
+             r'  { "name":"F", "parents":[]        }'  \
+             r']'
+
+data = json.loads(str(json_test2))
+print(data)
+
+my_dict = {}
+
+for cls in data:
+    parents = cls['parents']
+    for parent in parents:
+        if parent not in my_dict.keys():
+            my_dict[parents[0]] = 1
+        else:
+            my_dict[parents[0]] += 1
+
+print(my_dict)
